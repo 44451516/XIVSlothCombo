662c5bd54ae1be43bf5f4a8b75e712d2443dc498
x <x@x.com>
2025-12-17 19:27:40 +0800

    1

--- XIVSlothComboX/自动类/ConsumableChecker.cs
+++ XIVSlothComboX/自动类/ConsumableChecker.cs
@@ -74,12 +74,21 @@ namespace XIVSlothComboX.自动类
         {
             var action = item.ItemAction.Value;
 
-            var actionParams =
-                hq ? action.DataHQ : action.Data; // [0] = status, [1] = extra == ItemFood row, [2] = duration
-            if (actionParams[0] is not 48 and not 49)
-                return null; // not 'well fed' or 'medicated'
-
-            return Svc.Data.GetExcelSheet<ItemFood>()?.GetRow(actionParams[1]);
+            // [0] = status, [1] = extra == ItemFood row, [2] = duration
+            if (hq)
+            {
+                var actionParams = action.DataHQ; // Collection<ushort>
+                if (actionParams[0] is not 48 and not 49)
+                    return null; // not 'well fed' or 'medicated'
+                return Svc.Data.GetExcelSheet<ItemFood>()?.GetRow(actionParams[1]);
+            }
+            else
+            {
+                var actionParams = action.Data; // Collection<RowRef>
+                if (actionParams[0].RowId is not 48 and not 49)
+                    return null; // not 'well fed' or 'medicated'
+                return Svc.Data.GetExcelSheet<ItemFood>()?.GetRow(actionParams[1].RowId);
+            }
         }
 
 
@@ -124,7 +133,7 @@ namespace XIVSlothComboX.自动类
                 return false; // not 'other'
 
             var action = item.ItemAction.Value;
-            return action.RowOffset == 816 && action.Data[0] is 300 or 301 or 1751 or 5329;
+            return action.RowOffset == 816 && action.Data[0].RowId is 300 or 301 or 1751 or 5329;
         }
 
         internal static bool IsSquadronManual(Item item)
@@ -133,7 +142,7 @@ namespace XIVSlothComboX.自动类
                 return false; // not 'other'
 
             var action = item.ItemAction.Value;
-            return action.RowOffset == 816 && action.Data[0] is 2291 or 2292 or 2293 or 2294;
+            return action.RowOffset == 816 && action.Data[0].RowId is 2291 or 2292 or 2293 or 2294;
         }
 
 
